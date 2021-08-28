c6293d61f66a1409e5745964c09b6c8836a1d6aa
bonot9434 <bonot9434@gmail.com>
2021-08-28 09:13:14 +0000

    [fix]修正・一部部分テンプレート

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,25 +1,25 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
-    @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
-    if @book_comment.save
-       redirect_to book_path(@book)
-    else
-       @user = @book.user
-       @booknew = Book.new
-       render "books/show"
-    end
+	@book = Book.find(params[:book_id])
+	@book_comment = BookComment.new(book_comment_params)
+	@book_comment.book_id = @book.id
+	@book_comment.user_id = current_user.id
+	if @book_comment.save
+  	   redirect_to book_path(@book.id)
+	else
+	   render 'books/show'
+	end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+	@book = Book.find(params[:book_id])
+	book_comment = @book.book_comments.find(params[:id])
+	book_comment.destroy
+	redirect_to request.referer
   end
 
   private
-
   def book_comment_params
-    params.require(:book_comment).permit(:comment)
+	  params.require(:book_comment).permit(:comment)
   end
 end
