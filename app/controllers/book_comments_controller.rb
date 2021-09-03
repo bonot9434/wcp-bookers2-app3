cf81dc683f5e9a67d07ed101e589cb0561cd8dd5
bonot9434 <bonot9434@gmail.com>
2021-09-03 08:44:40 +0000

    [add]いいね・コメント非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,17 +5,19 @@ class BookCommentsController < ApplicationController
 	@book_comment.book_id = @book.id
 	@book_comment.user_id = current_user.id
 	if @book_comment.save
-  	   redirect_to book_path(@book.id)
+		 redirect_to book_path(@book.id)
 	else
+		 @user = @book.user
+	   @booknew = Book.new
 	   render 'books/show'
 	end
   end
 
   def destroy
 	@book = Book.find(params[:book_id])
+  @book_comment = BookComment.new
 	book_comment = @book.book_comments.find(params[:id])
 	book_comment.destroy
-	redirect_to request.referer
   end
 
   private
