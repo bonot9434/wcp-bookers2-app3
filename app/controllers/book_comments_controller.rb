e3b99d90e1a51557f0f509737e86c28ece2e71af
bonot9434 <bonot9434@gmail.com>
2021-09-04 13:11:57 +0000

    [fix]いいね・コメント非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,18 +4,14 @@ class BookCommentsController < ApplicationController
 	@book_comment = BookComment.new(book_comment_params)
 	@book_comment.book_id = @book.id
 	@book_comment.user_id = current_user.id
-	if @book_comment.save
-		 redirect_to book_path(@book.id)
-	else
-		 @user = @book.user
-	   @booknew = Book.new
-	   render 'books/show'
-	end
+	  if @book_comment.save
+	  else
+	    render 'books/show'
+	  end
   end
 
   def destroy
 	@book = Book.find(params[:book_id])
-  @book_comment = BookComment.new
 	book_comment = @book.book_comments.find(params[:id])
 	book_comment.destroy
   end
